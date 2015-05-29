54c071c530b9e988184eacb55a1aad3cb8bda605
Ray Alez <raymestalez@gmail.com>
2015-05-29 16:03:17 -0400

    Cleanup urls.py

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ http://fictionhub.io/
 
 Community for writers. My goal is to create the perfect website for people to share stories they write.
 
-Project is in active development. This is a very early version, not even alpha yet, just a prototype. 
+Project is in active development. This is a very early version, not even alpha yet, just a prototype.
+
+Project discussion:
+https://gitter.im/raymestalez/fictionhub
 
 ![fictionhub](https://raw.githubusercontent.com/raymestalez/fictionhub/master/fictionhub/static/img/fictionhub.png)
 
--- fictionhub/chaoslegion/urls.py
+++ fictionhub/chaoslegion/urls.py
@@ -3,43 +3,47 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.posts_hot, name='hot_posts'), # hot
+    # List posts
+    url(r'^$', views.posts_hot, name='hot_posts'),
     url(r'^new/$', views.posts_new, name='new_posts'),
-    url(r'^subscriptions/$', views.subscriptions, name='subscriptions'),    
     url(r'^top/(?P<slug>[^\.]+)/$', views.posts_top, name='top_posts'),
-    url(r'^hub/(?P<slug>[^\.]+)', views.hub_new, name='hub'),    
+    url(r'^hub/(?P<slug>[^\.]+)', views.hub_new, name='hub'),
+    url(r'^subscriptions/$', views.subscriptions, name='subscriptions'),    
+    # TODO: add sorting. View hot/top posts.
 
-    url(r'^post/(?P<slug>[^\.]+)/edit', views.post_edit, name='post_edit'),        
-    url(r'^post/(?P<slug>[^\.]+)', views.post, name='view_post'),
-    # url(r'^user/(?P<username>[^\.]+)/top', views.user_top, name='user_top'),
-    # url(r'^user/(?P<username>[^\.]+)/comments', views.user_comments, name='user_comments'),
+    # User Page
+    url(r'^user/(?P<username>[^\.]+)', views.user_new, name='user_new'), # new posts
     url(r'^user/(?P<username>[^\.]+)/about', views.about, name='about'),
     url(r'^user/(?P<username>[^\.]+)/subscribe', views.subscribe, name='subscribe'),
     url(r'^user/(?P<username>[^\.]+)/unsubscribe', views.unsubscribe, name='unsubscribe'),
     url(r'^preferences/$', views.user_prefs, name='preferences'),
-    url(r'^user/(?P<username>[^\.]+)', views.user_new, name='user_new'),
-    url(r'^about/$', views.about, name='about'),    
-
-    url(r'^story/$', views.story, name='story'),
-    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/edit$', views.chapter_edit),
-    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/up$', views.chapter_up),
-    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/down$', views.chapter_down),
-    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/delete$', views.chapter_delete),        
-    url(r'^story/(?P<story>[^\.]+)/edit$', views.story_edit),
-    url(r'^story/(?P<story>[^\.]+)/feed$', views.story_feed),
-    url(r'^story/add$', views.story_create),                
-    url(r'^story/(?P<story>[^\.]+)/add$', views.chapter_create),
-    url(r'^story-create/$', views.story_create),
-    url(r'^story/(?P<story>[^\.]+)/delete$', views.story_delete),  
-    url(r'^chapter/$', views.chapter, name='chapter'),
-
-    url(r'^submit/$', views.submit, name='submit'),
-    url(r'^prompt/$', views.prompt, name='prompt'),    
-
+    # TODO: View top/hot posts, view user comments
+    # url(r'^user/(?P<username>[^\.]+)/top', views.user_top, name='user_top'),
+    # url(r'^user/(?P<username>[^\.]+)/comments', views.user_comments, name='user_comments'),
+    
+    # Posts
+    url(r'^post/add$', views.submit, name='submit'),
+    url(r'^prompt/$', views.prompt, name='prompt'), 
+    url(r'^post/(?P<slug>[^\.]+)', views.post, name='view_post'),
+    url(r'^post/(?P<slug>[^\.]+)/edit', views.post_edit, name='post_edit'),
     url(r'^upvote/$', views.upvote),
     url(r'^downvote/$', views.downvote),        
 
+    # Stories
+    url(r'^story/add$', views.story_create),
+    url(r'^story/(?P<story>[^\.]+)/edit$', views.story_edit),
+    url(r'^story/(?P<story>[^\.]+)/delete$', views.story_delete),      
+    url(r'^story/(?P<story>[^\.]+)/add$', views.chapter_create),
+    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/edit$', views.chapter_edit),    
+    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/up$', views.chapter_up),
+    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/down$', views.chapter_down),
+    url(r'^story/(?P<story>[^\.]+)/(?P<chapter>[^\.]+)/delete$', views.chapter_delete),        
 
+    url(r'^story/(?P<story>[^\.]+)/feed$', views.story_feed),    
+    url(r'^story/$', views.story, name='story'),    
+    url(r'^chapter/$', views.chapter, name='chapter'),
+
+    # Auth
     url('^login/$', views.login_or_signup, name='login'),
     # url(r'^login/', 'django.contrib.auth.views.login', {'template_name':'auth/login.html'}),
     url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page':'/'}),
