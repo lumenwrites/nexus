6d1b3b996eb0d00403fde0260330186132f06de0
Ray Alez <raymestalez@gmail.com>
2015-06-05 05:25:04 -0400

    Update.

--- fictionhub/stories/templatetags/markdownify.py
+++ fictionhub/stories/templatetags/markdownify.py
@@ -8,14 +8,14 @@ register = template.Library()
 def markdownify(text):
     html = markdown.markdown(text)
 
-    linkify_html = bleach.linkify(html)
-    tags = ['img', 'p', 'em', 'strong', 'a', 'span', 'b', 'i', 'blockquote', 'hr'] # bleach.ALLOWED_TAGS
-    attributes = {
-        '*': ['class', 'style'],
-        'a': ['href', 'rel'],
-        'img': ['src', 'alt', 'style'],
-    }
-    styles = ['float','font-weight', 'width']
+    # linkify_html = bleach.linkify(html)
+    # tags = ['img', 'p', 'em', 'strong', 'a', 'span', 'b', 'i', 'blockquote', 'hr'] # bleach.ALLOWED_TAGS
+    # attributes = {
+    #     '*': ['class', 'style'],
+    #     'a': ['href', 'rel'],
+    #     'img': ['src', 'alt', 'style'],
+    # }
+    # styles = ['float','font-weight', 'width']
     
-    clean_html = bleach.clean(linkify_html, styles=styles, tags=tags, attributes=attributes, strip=True)
+    # clean_html = bleach.clean(linkify_html, styles=styles, tags=tags, attributes=attributes, strip=True)
     return html #clean_html
