865f6740aaca5a64c3d189992dc5ffa3b8b1b1b1
Ray Alez <raymestalez@gmail.com>
2015-06-05 04:18:18 -0400

    Sanitize html.

--- fictionhub/stories/templatetags/markdownify.py
+++ fictionhub/stories/templatetags/markdownify.py
@@ -1,9 +1,21 @@
 from django import template
 import markdown
+import bleach
  
 register = template.Library()
  
 @register.filter
 def markdownify(text):
-    # safe_mode governs how the function handles raw HTML
-    return markdown.markdown(text, safe_mode='False') 
+    html = markdown.markdown(text)
+
+    linkify_html = bleach.linkify(html)
+    tags = ['img', 'p', 'em', 'strong'] # bleach.ALLOWED_TAGS
+    attributes = {
+        '*': ['class', 'style'],
+        'A': ['href', 'rel'],
+        'img': ['src', 'alt', 'style'],
+    }
+    styles = ['float','font-weight', 'width']
+    
+    clean_html = bleach.clean(linkify_html, styles=styles, tags=tags, attributes=attributes, strip=True)
+    return clean_html
