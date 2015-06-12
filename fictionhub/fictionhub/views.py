54e0c03afd60ddc726cb36038f23caac958f2e90
Ray Alez <raymestalez@gmail.com>
2015-06-12 08:52:29 -0400

    Display Hubs.

--- fictionhub/fictionhub/views.py
+++ fictionhub/fictionhub/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 
 from stories.models import Story
 from stories.utils import rank_hot, rank_top
@@ -8,7 +9,7 @@ def home(request):
     stories = Story.objects.filter(published=True)    
     hot_stories = rank_hot(stories, top=32)[:10]
     new_stories = stories.order_by('-pub_date')[:10]
-    hubs = Hub.objects.all().order_by('id')
+    hubs = Hub.objects.all().annotate(number_of_stories=Count('stories')).order_by('-number_of_stories')
     return render(request, 'home.html', {
         'hot_stories':hot_stories,
         'new_stories':new_stories,
