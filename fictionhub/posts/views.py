c48cbbc10ecb264d60f3be9cc0a5e3e42e433945
Ray Alez <raymestalez@gmail.com>
2016-04-29 12:31:37 -0400

    Update.

--- fictionhub/posts/views.py
+++ fictionhub/posts/views.py
@@ -978,27 +978,6 @@ def post_create_daily(request):
 
 
 
-import time
-def post_to_reddit(request, story):
-    post = Post.objects.get(slug=story)
-
-    # throw him out if he's not an author
-    if request.user != post.author:
-        return HttpResponseRedirect('/')        
-
-
-    r = praw.Reddit(user_agent='Post /r/WritingPrompts story by /u/raymestalez')
-    r.login(os.environ["REDDITUNAME"],os.environ["REDDITUPASS"])
-    # subreddit = r.get_subreddit('WritingPrompts')
-    submission = r.get_submission(post.reddit_url)
-    submission.add_comment(post.body)
-    time.sleep(1)
-    
-    return HttpResponseRedirect(post.reddit_url)
-
-
-
-import praw    
 
     # prompts
 def writing_prompts(request):
