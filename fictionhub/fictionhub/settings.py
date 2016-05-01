a6a98e9a7e210379ecb8a42a6c406b48fc80b835
Ray Alez <raymestalez@gmail.com>
2016-05-01 08:29:04 -0400

    Update.

--- fictionhub/fictionhub/settings.py
+++ fictionhub/fictionhub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # False
+DEBUG = False # False
 
 ALLOWED_HOSTS = ["*",]
 
